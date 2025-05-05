3e34879499180e555c3f4dfdf0f18f48530f2025
whsung0330 <whsung0330@gmail.com>
2025-05-05 15:46:08 +0900

    init: 프로젝트 초기 설정

---
+++ src/main/java/api/PreonboardingChallengeApplication.java
@@ -0,0 +1,13 @@
+package api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreonboardingChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PreonboardingChallengeApplication.class, args);
+    }
+
+}
